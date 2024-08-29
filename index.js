4becdc6b6aacd49194cf20f0a980ca7c29f85a60
SamuelVitor02 <127554098+SamuelVitor02@users.noreply.github.com>
2024-08-29 01:27:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const primeiroNome = "Samuel";
+const sobreNome = "Goes";
+const idade = 16;
+const altura = 1.84;
+const massa = 65
+const cidadeNatal = "Curitiba";
+
+
+console.log("O meu primeiro nome é: ")
+console.log(primeiroNome);
+console.log("O meu sobre nome é: ")
+console.log(sobreNome);
+console.log("Minha idade:")
+console.log(idade);
+console.log("Minha Altura:")
+console.log(altura);
+console.log("Meu peso:")
+console.log(massa);
+console.log("Minha cidade natal:")
+console.log(cidadeNatal);
